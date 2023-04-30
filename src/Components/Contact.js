c04a03bdd0cea36506e75e0e300c35481b4e64e0
Felix Kipruto <kimosopfelix@gmail.com>
2023-04-30 18:23:30 +0300

    responsive contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,66 +2,56 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div className='flex text-center'>
-        <div style={{marginLeft:"90px"}}>
-            <h1 className='text-orange-500 text-center mt-20'>Contact Information</h1>
-            <div className='flex gap-10'>
-                <div>
+    <div className='flex flex-col md:flex-row justify-center text-center'>
+      <div className='md:ml-20'>
+        <h1 className='text-orange-500 text-center mt-20'>Contact Information</h1>
+        <div className='flex flex-col md:flex-row gap-10'>
+          <div>
             <div>
-                <h3 className='text-orange-500 mt-4'>Head Office</h3>
-                <p className='text-sm'>1st Floor Rm FF5</p>
-                <p className='text-sm'>Watersys Plaza</p>
-                <p className='text-sm'>P.o Box 16013-00100</p>
-                <p className='text-sm'>NAIROBI</p>
-                <p className='text-sm'>Tel: +254 716392180</p>
+              <h3 className='text-orange-500 mt-4'>Head Office</h3>
+              <p className='text-sm'>1st Floor Rm FF5</p>
+              <p className='text-sm'>Watersys Plaza</p>
+              <p className='text-sm'>P.o Box 16013-00100</p>
+              <p className='text-sm'>NAIROBI</p>
+              <p className='text-sm'>Tel: +254 716392180</p>
 
-
-                <p className='text-sm mt-4'>Globe Roundabout</p>
-                <p className='text-sm'>House No. 19</p>
-                <p className='text-sm'>Keekrock Road</p>
-                <p className='text-sm'>Nairobi</p>
-                <p className='text-sm'>info@competitivemanpower.com</p>
+              <p className='text-sm mt-4'>Globe Roundabout</p>
+              <p className='text-sm'>House No. 19</p>
+              <p className='text-sm'>Keekrock Road</p>
+              <p className='text-sm'>Nairobi</p>
+              <p className='text-sm'>info@competitivemanpower.com</p>
             </div>
-           
-                
-                
-           
-
+          </div>
+          <div>
             <div>
-                <h3 className='text-orange-500 mt-4'>Uganda Office</h3>
-                <p className='text-sm'>Beta House, 2
-nd floor Suite 2A</p>
-                <p className='text-sm'>Ntinda Kigowa</p>
-                <p className='text-sm'>P.O Box 26078</p>
-                <p className='text-sm'>Kampala</p>
-                <p className='text-sm'>Tel: +256 787111423</p>
-            </div>
+              <h3 className='text-orange-500 mt-4'>Uganda Office</h3>
+              <p className='text-sm'>Beta House, 2nd floor Suite 2A</p>
+              <p className='text-sm'>Ntinda Kigowa</p>
+              <p className='text-sm'>P.O Box 26078</p>
+              <p className='text-sm'>Kampala</p>
+              <p className='text-sm'>Tel: +256 787111423</p>
             </div>
             <div>
-
+              <h3 className='text-orange-500 mt-4'>Tanzania Office</h3>
+              <p className='text-sm'>Masela Plaza 3rd Floor</p>
+              <p className='text-sm'>Rumumba Avenue,</p>
+              <p className='text-sm'>Morogoro Road</p>
+              <p className='text-sm'>Suite 314, 316, 317</p>
+              <p className='text-sm'>P.O Box 751308</p>
+              <p className='text-sm'>Dar es salaam</p>
+              <p className='text-sm'>Tel: +255 717221221</p>
+            </div>
             <div>
-                <h3 className='text-orange-500 mt-4'>Tanzania Office</h3>
-                <p className='text-sm'>Masela Plaza 3rd Floor</p>
-                <p className='text-sm'>Rumumba Avenue,</p>
-                <p className='text-sm'>Morogoro Road</p>
-                <p className='text-sm'>Suite 314, 316, 317</p>
-                <p className='text-sm'>P.O Box 751308</p>
-                <p className='text-sm'>Dar es salaam</p>
-                <p className='text-sm'>Tel: +255 717221221</p>
+              <h3 className='text-orange-500 mt-4'>Dubai Office</h3>
+              <p className='text-sm'>Dubai Marina</p>
+              <p className='text-sm'>Jewellary &Complex</p>
+              <p className='text-sm'>DMCC, Dubai</p>
+              <p className='text-sm'>Mob. +971551677193/+971561950632</p>
+              <p className='text-sm'>Email: dubai@competivemanpower.com</p>
             </div>
-
-            <div>
-                <h3 className='text-orange-500 mt-4'>Dubai Office</h3>
-                <p className='text-sm'>Dubai Marina</p>
-                <p className='text-sm'>Jewellary &Complex</p>
-                <p className='text-sm'>DMCC, Dubai</p>
-                <p className='text-sm'>Mob. +971551677193/+971561950632</p>
-                <p className='text-sm'>Email: dubai@competivemanpower.com</p>
-            </div>
-            </div>
-            </div>
-
+          </div>
         </div>
+      </div>
     <div style={{marginLeft:"350px",width:"400px"}}>
         <section class="bg-white dark:bg-gray-900">
   <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
--- src/Components/Faqs.js
+++ src/Components/Faqs.js
@@ -2,72 +2,73 @@ import React, { useState } from 'react';
 import styles from './commons/style';
 
 function Faqs() {
-const [activeIndex, setActiveIndex] = useState(-1);
+  const [activeIndex, setActiveIndex] = useState(-1);
 
-const toggleActive = (index) => {
-if (activeIndex === index) {
-setActiveIndex(-1);
-} else {
-setActiveIndex(index);
-}
-};
+  const toggleActive = (index) => {
+    if (activeIndex === index) {
+      setActiveIndex(-1);
+    } else {
+      setActiveIndex(index);
+    }
+  };
 
-const faqs = [
-{
-question: '1. What is Competitive Manpower International Limited?',
-answer: 'Competitive Manpower International Limited is a Kenyan based company that provides manpower services.',
-},
-{
-question: '2. When was Competitive Manpower International Limited founded?',
-answer: 'Competitive Manpower International Limited was founded in 2010.',
-},
-{
-question: '3. Why was Competitive Manpower International Limited founded?',
-answer: 'Competitive Manpower International Limited was founded to meet the demand for manpower and address the high number of unemployed youth in Kenya.',
-},
-{
-question: '4. Where does Competitive Manpower International Limited operate?',
-answer: 'Competitive Manpower International Limited operates in Kenya, Uganda, Tanzania, and Ghana.',
-},
-{
-question: '5. What kind of team does Competitive Manpower International Limited have?',
-answer: 'Competitive Manpower International Limited has a close-knit organized team consisting of dynamic and talented achievers with professional backgrounds in administrative and human resources.',
-},
-{
-question: '6. What kind of services does Competitive Manpower International Limited provide?',
-answer: 'Competitive Manpower International Limited is a multiple service provider with professionals from various industries, and is built on over 5 years of experience in management and recruitment. The company provides quality services and solutions in manpower, among other services.',
-},
-{
-question: '7. How long has the management and employees of Competitive Manpower International Limited been in the industry?',
-answer: 'The management and employees of Competitive Manpower International Limited have been in the industry for over 5 years.',
-},
-{
-question: '8. What is the intention of Competitive Manpower International Limited in providing their services?',
-answer: 'The intention of Competitive Manpower International Limited is to provide quality services at a standard unmatched by others.',
-},
-{
-question: '9. How can customers contact Competitive Manpower International Limited?',
-answer: 'Customers can contact Competitive Manpower International Limited for more details and to utilize their services through their website or by email/phone.',
-},
-];
+  const faqs = [
+    {
+      question: '1. What is Competitive Manpower International Limited?',
+      answer:
+        'Competitive Manpower International Limited is a Kenyan based company that provides manpower services.',
+    },
+    {
+      question: '2. When was Competitive Manpower International Limited founded?',
+      answer: 'Competitive Manpower International Limited was founded in 2010.',
+    },
+    {
+      question: '3. Why was Competitive Manpower International Limited founded?',
+      answer:
+        'Competitive Manpower International Limited was founded to meet the demand for manpower and address the high number of unemployed youth in Kenya.',
+    },
+    {
+      question: '4. Where does Competitive Manpower International Limited operate?',
+      answer: 'Competitive Manpower International Limited operates in Kenya, Uganda, Tanzania, and Ghana.',
+    },
+    {
+      question: '5. What kind of team does Competitive Manpower International Limited have?',
+      answer:
+        'Competitive Manpower International Limited has a close-knit organized team consisting of dynamic and talented achievers with professional backgrounds in administrative and human resources.',
+    },
+    {
+      question: '6. What kind of services does Competitive Manpower International Limited provide?',
+      answer:
+        'Competitive Manpower International Limited is a multiple service provider with professionals from various industries, and is built on over 5 years of experience in management and recruitment. The company provides quality services and solutions in manpower, among other services.',
+    },
+    {
+      question: '7. How long has the management and employees of Competitive Manpower International Limited been in the industry?',
+      answer: 'The management and employees of Competitive Manpower International Limited have been in the industry for over 5 years.',
+    },
+    {
+      question: '8. What is the intention of Competitive Manpower International Limited in providing their services?',
+      answer: 'The intention of Competitive Manpower International Limited is to provide quality services at a standard unmatched by others.',
+    },
+    {
+      question: '9. How can customers contact Competitive Manpower International Limited?',
+      answer: 'Customers can contact Competitive Manpower International Limited for more details and to utilize their services through their website or by email/phone.',
+    },
+  ];
 
-return (
-<div className="p-6 " style={{width:"1000px", marginLeft:"200px"}}>
-<h2 className="text-xl font-bold  text-orange-500 mb-4 text-center">Frequently Asked Questions</h2>
-{faqs.map((faq, index) => (
-<div
-key={index}
-className="border-b pb-6 mb-6 cursor-pointer"
-onClick={() => toggleActive(index)}
->
-<h3 className="text-lg font-bold mb-2">{faq.question}</h3>
-{activeIndex === index && (
-<p className={`${styles.paragraph} text-sm px-4 mb-2 text-primary`}>{faq.answer}</p>
-)}
-</div>
-))}
-</div>
-);
+  return (
+    <div className="p-6 " style={{ maxWidth: '1000px', margin: 'auto' }}>
+      <h2 className="text-xl font-bold text-orange-500 mb-4 text-center">Frequently Asked Questions</h2>
+      {faqs.map((faq, index) => (
+        <div key={index} className="border-b pb-6 mb-6 cursor-pointer" onClick={() => toggleActive(index)}>
+          <h3 className="text-lg font-bold mb-2">{faq.question}</h3>
+          {activeIndex === index && (
+            <p className={`${styles.paragraph} text-sm px-4 mb-2 text-primary`}>{faq.answer}</p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Faqs;
+
